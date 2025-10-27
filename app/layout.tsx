f8802192609c442ed829b37b93b779030573d030
edwxrdbobb <edwardbobkamara@gmail.com>
2025-10-27 03:16:08 +0000

    footer fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import ReduxProvider from '@/components/ReduxProvider';
 import { AuthProvider } from '@/components/auth-context';
 import { ThemeProvider } from '@/components/theme-provider';
 
-import Navbar from "@/components/Navbar"
-import Footer from "@/components/Footer"
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ['latin'] });
 
