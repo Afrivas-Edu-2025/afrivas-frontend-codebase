8a0e689a3e4c27aea407645e1bdd5e670c032d58
Edward <edwardbobkamara@gmail.com>
2026-01-11 02:04:01 +0000

    feat: move backend URL to environment variables

--- services/authServices.ts
+++ services/authServices.ts
@@ -33,7 +33,7 @@ export type LoginUserRequest = {
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5050/api/v1',
+    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem('authToken');
       if (token) {
